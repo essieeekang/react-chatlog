6265861f369d26f7260e1a30e212e589d99025ef
Esther Kang <estherkang.j@gmail.com>
2025-06-15 14:18:39 -0700

    renders one chat entry successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import DATA from './data/messages.json';
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <div><ChatEntry sender={DATA[0].sender} body={DATA[0].body} timeStamp={DATA[0].timeStamp}/></div>
       </main>
     </div>
   );
